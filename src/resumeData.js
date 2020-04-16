94fc3dccd534ee46cda165218dba809871b4ab06
Isaac-Aguirre <isaac42700@gmail.com>
2020-04-15 21:42:53 -0700

    added the files to the repository

---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Isaac Aguirre",
+    "role": "Full Stack developer",
+    "linkedinId":"Isaac Aguirre",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I love learning about full stack development. In my free time i am working on new projects to add to my portfolio and improve on my skills.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/isaac-aguirre-65202b199",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Isaac-Aguirre",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"#",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently still enrolled into El Camino College, I took a semester off and did a coding bootcamp at the UCLA extension. There i learned the fundementals to being a full stack developer. I have lots of intrest in either front or backend work. I am eager to learn new technoligies, and I am still building on my portfolio with new projects",
+    "address":"Torrance, CA",
+    "website":"https://github.com/Isaac-Aguirre",
+    "education":[
+      {
+        "UniversityName":"UCLA Extension Bootcamp",
+        "specialization":"Full Stack development",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2020",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"El Camino",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Enrolled",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Trader Joe's",
+        "specialization":"",
+        "MonthOfLeaving":"Still Working",
+        "YearOfLeaving":"Still Working",
+        "Achievements":""
+      },
+      // {
+      //   "CompanyName":"Some Company",
+      //   "specialization":"Some specialization",
+      //   "MonthOfLeaving":"Jan",
+      //   "YearOfLeaving":"2018",
+      //   "Achievements":"Some Achievements"
+      // }
+    ],
+    "skillsDescription":"My skills include",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname": "Node.js"
+      },
+      {
+        "skillname": "Several node libraries"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"",
+        "description":"",
+        "imgurl":""
+      },
+      {
+        "name":"",
+        "description":"",
+        "imgurl":""
+      },
+      {
+        "name":"",
+        "description":"",  
+        "imgurl":""
+      },
+      {
+        "name":"",
+        "description":"",
+        "imgurl":""
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"",
+        "name":""
+      },
+      {
+        "description":"",
+        "name":""
+      }
+    ]
+  }
+  
+  export default resumeData
