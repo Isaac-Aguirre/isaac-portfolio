d37cba9203a34e0e3e95fa179d75f53f8602553d
Isaac-Aguirre <isaac42700@gmail.com>
2020-06-19 16:15:16 -0700

    Updates

---
+++ precache-manifest.0741f6b67f8e5a43f2e511354ca4a9c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f69c1e8a0d43479cb855",
+    "url": "/isaac-portfolio/static/css/main.ade4d4d9.chunk.css"
+  },
+  {
+    "revision": "f69c1e8a0d43479cb855",
+    "url": "/isaac-portfolio/static/js/main.f69c1e8a.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/isaac-portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "44f646ee7d7bba745d05",
+    "url": "/isaac-portfolio/static/js/runtime~main.44f646ee.js"
+  },
+  {
+    "revision": "567ca8f522b2be3436a2137b9f70c809",
+    "url": "/isaac-portfolio/index.html"
+  }
+];
